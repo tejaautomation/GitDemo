7b4e82f0c202672ade45dc422a504e81a194b4cc
tejaautomation <teja981992@gmail.com>
2020-05-14 17:47:38 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	
+	@Test
+	public void MobileAutomate() {
+		
+		System.out.println("MobileAutomate");
+		
+	}
+
+}
---
+++ src/test/java/qaclickacademy/seleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	@Test
+	public void BrowserAutomation() {
+		
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	
+	public void ElementUi() {
+		
+		
+		System.out.println("ElementUi");
+	}
+}
